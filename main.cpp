e09389b029ad823589e670210ffa612cd69d24c9
FryingDutch <abbring_93@outlook.com>
2021-09-04 17:04:37 +0200

    Add showcasings

--- main.cpp
+++ main.cpp
@@ -5,12 +5,30 @@ struct FiveBooleanByte
 {
 	typedef unsigned char byte;
 
+public:
+	// Set a union to make it possible to call the bits on a different naming, without taking up more memory.
 	union
 	{
+		byte bits;
+		byte counter;
 		byte booleans;
 		byte values;
 		byte ports;
 	};
+
+public:
+	// Overload operators to make it possible to compare FiveBooleanBytes with one another.
+	bool operator==(const FiveBooleanByte& rhs)
+	{
+		if (this->values == rhs.values) return true;
+		return false;
+	}
+
+	bool operator!=(const FiveBooleanByte& rhs)
+	{
+		if (this->values != rhs.values) return true;
+		return false;
+	}
 
 
 public:
@@ -21,38 +39,79 @@ public:
 
 	void add(bool _boolean)
 	{
-		//bitshift the 3 most significant bits completly to the right and check if their numerical value is lower then 5 (bitcounter)
-		if (booleans >> 5 < 5)
+		//bitshift the 3 most significant bits completely to the right and check if their numerical value is lower then 5 (bitcounter)
+		if (bits >> 5 < 5)
 		{
-			// Bitshift one to left 
-			// Add 0b0(false) or 0b1(true)
-			// Mask away the counter (3most significant bits) of this current result which is no langer valid due to the bitshift to the left. 
+			// Bitshift one to left to make room at the least significant bit for the new value.
+			// Add 0b0(false) or 0b1(true) at the least significant bit.
+			// Mask away the counter (3 most significant bits) of this current result which is no langer valid due to the bitshift to the left. 
 			// Then mask the values of this result with the actual state of the booleans counter (the 3 most significant bits) to add them together.
-			if (_boolean) this->booleans = (((this->booleans << 1) + 0b1) & 0b00'01'11'11) | (this->booleans & 0b11'10'00'00);
-			else this->booleans = (((this->booleans << 1) + 0b0) & 0b00'01'11'11) | (this->booleans & 0b11'10'00'00);
+			if (_boolean) this->bits = (((this->booleans << 1) + 0b1) & 0b00'01'11'11) | (this->counter & 0b11'10'00'00);
+			else this->bits = (((this->booleans << 1) + 0b0) & 0b00'01'11'11) | (this->counter & 0b11'10'00'00);
 
-			// Mask away the values to get the state of the counter (3 most signifcant bits), bitshift 5 to get its numerical value, add one (to counter) and bitshift them back
-			// to their original place. Then, use the current state of the booleans to mask away and add the results back together.
-			this->booleans = (((this->booleans >> 5) + 1) << 5) | (this->booleans & 0b00'01'11'11);
+			std::bitset<8> a(values);
+			std::cout << "After adding the value: " << a << "\n";
+
+			// Bitshift 5 to get the counters (3most significant bits) numerical value, add one (to counter) and bitshift them back to their original place.
+			// Then, use the current state of the bits to mask away the old counter (3 most significant bits of the original value) and add the results back together.
+			this->bits = (((this->counter >> 5) + 1) << 5) | (this->booleans & 0b00'01'11'11);
+
+			std::bitset<8> b(values);
+			std::cout << "After incrementing the counter: " << b << "\n\n";
 		}
 	}
 
-	void empty()
+	void clear()
 	{
 		this->booleans = 0b00'00'00'00;
 	}
 };
 
-FiveBooleanByte bools;
-
 int main(void)
 {
-	bools.add(true);
-	bools.add(true);
-	bools.add(false);
-	bools.add(true);
-	bools.add(true);
+	FiveBooleanByte bools;
+
+	bools.add(true);  // 1
+	bools.add(true);  // 2
+	bools.add(false); // 3
+	bools.add(true);  // 4
+	bools.add(true);  // 5
+
+	// Will not do anything because the counter is at 5
+	bools.add(false); // 6
 
 	std::bitset<8> z(bools.values);
 	std::cout << "End: " << z << "\n";
+	std::cout << "The size of FIveBooleanByte (without counting its functions): " << sizeof(FiveBooleanByte) << " Byte(s).\n";
+
+	// Uncomment for the second part of the showcase.
+	/*
+	FiveBooleanByte bools2;
+
+	bools2.add(true);  // 1
+	bools2.add(true);  // 2
+	bools2.add(false); // 3
+	bools2.add(true);  // 4
+	bools2.add(true);  // 5
+
+	//Only for showcasing purposes, this is to clear the screen of all previous prints.
+	system("CLS");
+
+	//compare 2nd FiveBooleanByte with the first.
+	if(bools2 == bools) std::cout << "First and second FiveBooleanByte are equal.\n";
+
+	//clear bools and compare again.
+	bools.clear();
+	if(bools != bools2) std::cout << "First and second FiveBooleanByte are not equal.\n";
+	*/
+
+	// Uncomment for the third part of the showcase.
+	/*
+	system("CLS");
+
+	// A way for the user to extract single bits to check their value.
+	if ((bools.booleans & 0b00'00'01'00) >> 2 == false) std::cout << "This one bit is false\n";
+	if ((bools.booleans & 0b00'00'00'01) == true) std::cout << "This bit is true\n";
+	*/
+	
 }
