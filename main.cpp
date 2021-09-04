a7b564a0681c54f923cacf7fd11d70b7564c3a90
FryingDutch <abbring_93@outlook.com>
2021-09-04 18:53:04 +0200

    delete if statement

--- main.cpp
+++ main.cpp
@@ -34,25 +34,25 @@ public:
 public:
 	//start with an empty byte
 	FiveBooleanByte() :
-		booleans(0b0'0'0'0'0'0'0'0) {
+		bits(0b0'0'0'0'0'0'0'0) {
 	}
 
+	// Add a bool at the least significant bit
 	void add(bool _boolean)
 	{
 		//bitshift the 3 most significant bits completely to the right and check if their numerical value is lower then 5 (bitcounter)
-		if (bits >> 5 < 5)
+		if (this->counter >> 5 < 5)
 		{
 			// Bitshift one to left to make room at the least significant bit for the new value.
-			// Add 0b0(false) or 0b1(true) at the least significant bit.
+			// Add 0(false) or 1(true) at the least significant bit.
 			// Mask away the counter (3 most significant bits) of this current result which is no langer valid due to the bitshift to the left. 
-			// Then mask the values of this result with the actual state of the booleans counter (the 3 most significant bits) to add them together.
-			if (_boolean) this->bits = (((this->booleans << 1) + 0b1) & 0b00'01'11'11) | (this->counter & 0b11'10'00'00);
-			else this->bits = (((this->booleans << 1) + 0b0) & 0b00'01'11'11) | (this->counter & 0b11'10'00'00);
+			// Then mask the values of this result with the actual state of the counter bits (the 3 most significant bits) to add them together.
+			this->bits = (((this->booleans << 1) + _boolean) & 0b00'01'11'11) | (this->counter & 0b11'10'00'00);
 
 			std::bitset<8> a(values);
 			std::cout << "After adding the value: " << a << "\n";
 
-			// Bitshift 5 to get the counters (3most significant bits) numerical value, add one (to counter) and bitshift them back to their original place.
+			// Bitshift 5 to the right get the counters (3most significant bits) numerical value, add one (to counter) and bitshift them back to their original place.
 			// Then, use the current state of the bits to mask away the old counter (3 most significant bits of the original value) and add the results back together.
 			this->bits = (((this->counter >> 5) + 1) << 5) | (this->booleans & 0b00'01'11'11);
 
@@ -63,7 +63,7 @@ public:
 
 	void clear()
 	{
-		this->booleans = 0b00'00'00'00;
+		this->bits = 0b00'00'00'00;
 	}
 };
 
