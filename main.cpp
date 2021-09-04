7cc8df477295cb527ef4b955bdecefd793a88088
FryingDutch <abbring_93@outlook.com>
2021-09-04 15:43:29 +0200

    Removed local variables

--- main.cpp
+++ main.cpp
@@ -4,100 +4,42 @@
 struct FiveBooleanByte
 {
 	typedef unsigned char byte;
-	byte booleans;
+
+	union
+	{
+		byte booleans;
+		byte values;
+		byte ports;
+	};
+
 
 public:
-	//provide an empty byte
+	//start with an empty byte
 	FiveBooleanByte() :
-		booleans(0b0'0'0'0'0'0'0'0){
+		booleans(0b0'0'0'0'0'0'0'0) {
 	}
 
 	void add(bool _boolean)
 	{
-		byte counter = booleans >> 5;
-
-		if (counter < 5)
+		//bitshift the 3 most significant bits completly to the right and check if their numerical value is lower then 5 (bitcounter)
+		if (booleans >> 5 < 5)
 		{
-			byte result;
-			if (_boolean) result = 0b1;
-			else result = 0b0;
+			// Bitshift one to left 
+			// Add 0b0(false) or 0b1(true)
+			// Mask away the counter (3most significant bits) of this current result which is no langer valid due to the bitshift to the left. 
+			// Then mask the values of this result with the actual state of the booleans counter (the 3 most significant bits) to add them together.
+			if (_boolean) this->booleans = (((this->booleans << 1) + 0b1) & 0b00'01'11'11) | (this->booleans & 0b11'10'00'00);
+			else this->booleans = (((this->booleans << 1) + 0b0) & 0b00'01'11'11) | (this->booleans & 0b11'10'00'00);
 
-			this->booleans <<= 1;
-			this->booleans += result;
-			this->booleans &= 0b00'01'11'11;
-			counter++;
-			counter <<= 5;
-
-			this->booleans |= counter;
+			// Mask away the values to get the state of the counter (3 most signifcant bits), bitshift 5 to get its numerical value, add one (to counter) and bitshift them back
+			// to their original place. Then, use the current state of the booleans to mask away and add the results back together.
+			this->booleans = (((this->booleans >> 5) + 1) << 5) | (this->booleans & 0b00'01'11'11);
 		}
 	}
 
-	void remove(byte index)
+	void empty()
 	{
-		byte counter = booleans >> 5;
-		byte mask;
-
-		if (counter > 0)
-		{
-			switch (index)
-			{
-			case 1:
-				booleans &= 0b11'10'11'11;
-				break;
-
-			case 2:
-				if (counter > index)
-				{
-					booleans &= 0b00'01'01'11;
-					mask = (0b00'01'00'00 & booleans) >> 1;
-					booleans &= 0b00'00'01'11;
-					booleans |= mask;					
-				}
-
-				else booleans &= 0b00'00'01'11;
-				break;
-
-			case 3:
-				if (counter > index)
-				{
-					booleans &= 0b00'01'10'11;
-					mask = (0b00'01'10'00 & booleans) >> 1;
-					booleans &= 0b00'00'00'11;
-					booleans |= mask;
-				}
-
-				else booleans &= 0b00'00'00'11;
-
-			case 4:
-				if (counter > index)
-				{
-					booleans &= 0b00'01'11'01;
-					mask = (0b00'01'11'00 & booleans) >> 1;
-					booleans &= 0b00'00'00'01;
-					booleans |= mask;
-				}
-
-				else booleans &= 0b00'00'00'01;
-
-			case 5:
-				if (counter > index)
-				{
-					booleans &= 0b00'01'11'10;
-					mask = (0b00'01'11'10 & booleans) >> 1;
-					booleans &= 0b00'00'00'00;
-					booleans |= mask;
-				}
-
-				else booleans &= 0b00'00'00'00;
-
-			default:
-				break;
-			}
-
-			counter--;
-			counter <<= 5;
-			booleans |= counter;
-		}
+		this->booleans = 0b00'00'00'00;
 	}
 };
 
@@ -107,15 +49,10 @@ int main(void)
 {
 	bools.add(true);
 	bools.add(true);
-	bools.add(true);
+	bools.add(false);
 	bools.add(true);
 	bools.add(true);
 
-	std::bitset<8> r(bools.booleans);
-	std::cout << r << "\n";
-
-	bools.remove(4);
-	
-	std::bitset<8> z(bools.booleans);
-	std::cout << z;
+	std::bitset<8> z(bools.values);
+	std::cout << "End: " << z << "\n";
 }
