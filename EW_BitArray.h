d99f13ac282a43ad76de53081190456f004cf8cc
FryingDutch <abbring_93@outlook.com>
2021-09-15 22:03:15 +0200

    guarantee byte sizes

--- EW_BitArray.h
+++ EW_BitArray.h
@@ -1,6 +1,7 @@
 // FiveBooleanByte is library independend
 #include <iostream> // Needed for logging and debugging
 #include <bitset> // Needed to show the byte in a binary representation
+#include <stdexcept>
 
 template<typename T>
 struct EW_BitArray
@@ -19,300 +20,284 @@ private:
 	};
 
 public:
-	// Overload operator to make it possible to compare FiveBooleanBytes with one another without taking the counterbits into factor.
+	// Overload operator to make it possible to compare EW_BitArrays with one another without taking the counterbits into factor.
 	bool operator==(const EW_BitArray& rhs)
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			return (this->values & 31) == (rhs.values & 31);
+			return (values & 31) == (rhs.values & 31);
 
 		case 2:
-			return (this->values & 4095) == (rhs.values & 4095);
+			return (values & 4095) == (rhs.values & 4095);
 
 		case 4:
-			return (this->values & 67108863) == (rhs.values & 67108863);
+			return (values & 67108863) == (rhs.values & 67108863);
 
 		case 8:
-			return (this->values & 0x01FFFFFFFFFFFFFF) == (rhs.values & 0x01FFFFFFFFFFFFFF);
-
-		default:
-			return false;
+			return (values & 0x01FFFFFFFFFFFFFF) == (rhs.values & 0x01FFFFFFFFFFFFFF);
 		}
 	}
 
-	// Overload operator to make it possible to compare FiveBooleanBytes with one another without taking the counterbits into factor.
+	// Overload operator to make it possible to compare EW_BitArrays with one another without taking the counterbits into factor.
 	bool operator!=(const EW_BitArray& rhs)
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			return (this->values & 31) != (rhs.values & 31);
+			return (values & 31) != (rhs.values & 31);
 
 		case 2:
-			return (this->values & 4095) != (rhs.values & 4095);
+			return (values & 4095) != (rhs.values & 4095);
 
 		case 4:
-			return (this->values & 67108863) != (rhs.values & 67108863);
+			return (values & 67108863) != (rhs.values & 67108863);
 
 		case 8:
-			return (this->values & 0x01FFFFFFFFFFFFFF) != (rhs.values & 0x01FFFFFFFFFFFFFF);
-
-		default:
-			return false;
+			return (values & 0x01FFFFFFFFFFFFFF) != (rhs.values & 0x01FFFFFFFFFFFFFF);
 		}
 	}
 
-	int operator[](std::size_t _index)
+	int16_t operator[](std::size_t _index)
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			if (_index < (this->counter >> 5) && _index < 5 && _index >= 0) 
-				return this->bits >> (((this->counter >> 5) - 1) - _index) & 1;
-			else return -1;
+			if (_index < (counter >> 5) && _index < 5 && _index >= 0) 
+				return bits >> (((counter >> 5) - 1) - _index) & 1;
+			else throw std::out_of_range("Index out of bounds");
+			return -1;
 
 		case 2:
-			if (_index < (this->counter >> 12) && _index < 12 && _index >= 0) 
-				return this->bits >> (((this->counter >> 12) - 1) - _index) & 1;
-			else return -1;
+			if (_index < (counter >> 12) && _index < 12 && _index >= 0) 
+				return bits >> (((counter >> 12) - 1) - _index) & 1;
+			else throw std::out_of_range("Index out of bounds");
+			return -1;
 
 		case 4:
-			if (_index < (this->counter >> 27) && _index < 27 && _index >= 0) 
-				return this->bits >> (((this->counter >> 27) - 1) - _index) & 1;
-			else return -1;
+			if (_index < (counter >> 27) && _index < 27 && _index >= 0) 
+				return bits >> (((counter >> 27) - 1) - _index) & 1;
+			else throw std::out_of_range("Index out of bounds");
+			return -1;
 
 		case 8:
-			if (_index < (this->counter >> 58) && _index < 58 && _index >= 0) 
-				return this->bits >> (((this->counter >> 58) - 1) - _index) & 1;
-			else return -1;
+			if (_index < (counter >> 58) && _index < 58 && _index >= 0) 
+				return bits >> (((counter >> 58) - 1) - _index) & 1;
+			else throw std::out_of_range("Index out of bounds");
+			return -1;
 
 		default:
 			return -1;
 		}
 	}
 
-	// Overload operator to make it possible to copy FiveBooleanBytes into another.
+	// Overload operator to make it possible to copy EW_BitArrays into one another.
 	void operator=(const EW_BitArray& rhs)
 	{
-		this->values = rhs.values;
+		values = rhs.values;
 	}
 
 public:
 	// Start with an empty byte or an already filled in one. Keep in mind that you'll also have to manually input the counter correctly in that case.
-	EW_BitArray(bytes _bits = 0x00, T _size = 0)
+	EW_BitArray(T _size = 0, bytes _bits = 0x00)
 	{
 		if (_size == 0) _bits = 0x00;
 
 		switch (sizeof(T))
 		{
 		case 1:
-			this->bits = (_bits & 31) | (_size << 5);
+			bits = (_bits & 31) | (_size << 5);
 			break;
 
 		case 2:
-			this->bits = (_bits & 4095) | (_size << 12);
+			bits = (_bits & 4095) | (_size << 12);
 			break;
 
 		case 4:
-			this->bits = (_bits & 67108863) | (_size << 27);
+			bits = (_bits & 67108863) | (_size << 27);
 			break;
 
 		case 8:
-			this->bits = (_bits & 0x01FFFFFFFFFFFFFF) | ((unsigned long long)_size << 58);
+			bits = (_bits & 0x01FFFFFFFFFFFFFF) | ((uint64_t)_size << 58);
 			break;
 
 		default:
+			throw std::logic_error("No bits allocated");
 			break;
 		}
 	}
 
 	// Add a bool at the least significant bit
-	void push_back(const bool _boolean)
+	void add(const bool _boolean) noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			if (this->counter >> 5 < 5) 
-				this->bits = (((this->booleans << 1) + _boolean) & 31) | (((this->counter >> 5) + 1) << 5);
+			if (counter >> 5 < 5) 
+				bits = (((booleans << 1) + _boolean) & 31) | (((counter >> 5) + 1) << 5);
 			break;
 
 		case 2:
-			if (this->counter >> 12 < 12) 
-				this->bits = (((this->booleans << 1) + _boolean) & 4095) | (((this->counter >> 12) + 1) << 12);
+			if (counter >> 12 < 12) 
+				bits = (((booleans << 1) + _boolean) & 4095) | (((counter >> 12) + 1) << 12);
 			break;
 
 		case 4:
-			if (this->counter >> 27 < 27) 
-				this->bits = (((this->booleans << 1) + _boolean) & 67108863) | (((this->counter >> 27) + 1) << 27);
+			if (counter >> 27 < 27) 
+				bits = (((booleans << 1) + _boolean) & 67108863) | (((counter >> 27) + 1) << 27);
 			break;
 
 		case 8:
-			if (this->counter >> 58 < 58) 
-				this->bits = (((T)((T)this->booleans << 1) + (T)_boolean) & (T)0x01FFFFFFFFFFFFFF) | (((unsigned long long)(this->counter >> 58) + 1) << 58);
-			break;
-
-		default:
+			if (counter >> 58 < 58) 
+				bits = (((T)((T)booleans << 1) + (T)_boolean) & (T)0x01FFFFFFFFFFFFFF) | (((uint64_t)(counter >> 58) + 1) << 58);
 			break;
 		}
 
 		std::bitset<sizeof(T) * 8> a(values);
 		std::cout << "After adding the value: " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// Remove last element
-	void pop_back()
+	void pop_back() noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			if (this->counter >> 5 > 0) 
-				this->bits = ((this->booleans & 31) >> 1) | (((this->counter >> 5) - 1) << 5);
+			if (counter >> 5 > 0) 
+				bits = ((booleans & 31) >> 1) | (((counter >> 5) - 1) << 5);
 			break;
 
 		case 2:
-			if (this->counter >> 12 > 0) 
-				this->bits = ((this->booleans & 4095) >> 1) | (((this->counter >> 12) - 1) << 12);
+			if (counter >> 12 > 0) 
+				bits = ((booleans & 4095) >> 1) | (((counter >> 12) - 1) << 12);
 			break;
 
 		case 4:
-			if (this->counter >> 27 > 0) 
-				this->bits = ((this->booleans & 67108863) >> 1) | (((this->counter >> 27) - 1) << 27);
+			if (counter >> 27 > 0) 
+				bits = ((booleans & 67108863) >> 1) | (((counter >> 27) - 1) << 27);
 			break;
 
 		case 8:
-			if (this->counter >> 58 > 0) 
-				this->bits = ((this->booleans & (T)0x01FFFFFFFFFFFFFF) >> 1) | (((unsigned long long)(this->counter >> 58) - 1) << 58);
-			break;
-
-		default:
+			if (counter >> 58 > 0) 
+				bits = ((booleans & (T)0x01FFFFFFFFFFFFFF) >> 1) | (((uint64_t)(counter >> 58) - 1) << 58);
 			break;
 		}
 
 		std::bitset<sizeof(T) * 8> a(values);
 		std::cout << "After removing the least significant bit: " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// Remove first element
-	void pop_front()
+	void pop_front() noexcept
 	{
 		//first counter is to find the position of the to be removed bit, second is to decrement the counter.
 		switch (sizeof(T))
 		{
 		case 1:
-			this->bits = (this->booleans & (T)((T)pow(2, (this->counter >> 5) - 1) - 1)) | (((this->counter >> 5) - 1) << 5);
+			bits = (booleans & (T)((T)pow(2, (counter >> 5) - 1) - 1)) | (((counter >> 5) - 1) << 5);
 			break;
 
 		case 2:
-			this->bits = (this->booleans & (T)((T)pow(2, (this->counter >> 12) - 1) - 1)) | (((this->counter >> 12) - 1) << 12);
+			bits = (booleans & (T)((T)pow(2, (counter >> 12) - 1) - 1)) | (((counter >> 12) - 1) << 12);
 			break;
 
 		case 4:
-			this->bits = (this->booleans & (T)((T)pow(2, (this->counter >> 27) - 1) - 1)) | (((this->counter >> 27) - 1) << 27);
+			bits = (booleans & (T)((T)pow(2, (counter >> 27) - 1) - 1)) | (((counter >> 27) - 1) << 27);
 			break;
 
 		case 8:
-			this->bits = (this->booleans & (T)((T)pow(2, (this->counter >> 58) - 1) - 1)) | (((unsigned long long)(this->counter >> 58) - 1) << 58);
-			break;
-
-		default:
+			bits = (booleans & (T)((T)pow(2, (counter >> 58) - 1) - 1)) | (((uint64_t)(counter >> 58) - 1) << 58);
 			break;
 		}
 
 		std::bitset<sizeof(T) * 8> a(values);
 		std::cout << "After removing the most significant bit: " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// Remove element at position
-	void erase(const unsigned char _position)
+	void remove(const uint8_t _position) noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			if (this->counter >> 5 >= _position) 
-				this->bits = ((((this->booleans & (((T)31 >> ((this->counter >> 5) - _position)) << ((this->counter >> 5) - _position)))) >> 1) | (this->booleans & (char)(pow(2, (this->counter >> 5) - (_position + 1)) - 1))) | (((this->counter >> 5) - 1) << 5);
+			if (counter >> 5 >= _position) 
+				bits = ((((booleans & (((T)31 >> ((counter >> 5) - _position)) << ((counter >> 5) - _position)))) >> 1) | (booleans & (int8_t)(pow(2, (counter >> 5) - (_position + 1)) - 1))) | (((counter >> 5) - 1) << 5);
 			break;
 
 		case 2:
-			if (this->counter >> 12 >= _position)  
-				this->bits = ((((this->booleans & (((T)4095 >> ((this->counter >> 12) - _position)) << ((this->counter >> 12) - _position)))) >> 1) | (this->booleans & (char)(pow(2, (this->counter >> 12) - (_position + 1)) - 1))) | (((this->counter >> 12) - 1) << 12);
+			if (counter >> 12 >= _position)  
+				bits = ((((booleans & (((T)4095 >> ((counter >> 12) - _position)) << ((counter >> 12) - _position)))) >> 1) | (booleans & (int8_t)(pow(2, (counter >> 12) - (_position + 1)) - 1))) | (((counter >> 12) - 1) << 12);
 			break;
 
 		case 4:
-			if (this->counter >> 27 >= _position)  
-				this->bits = ((((this->booleans & (((T)67108863 >> ((this->counter >> 27) - _position)) << ((this->counter >> 27) - _position)))) >> 1) | (this->booleans & (char)(pow(2, (this->counter >> 27) - (_position + 1)) - 1))) | (((this->counter >> 27) - 1) << 27);
+			if (counter >> 27 >= _position)  
+				bits = ((((booleans & (((T)67108863 >> ((counter >> 27) - _position)) << ((counter >> 27) - _position)))) >> 1) | (booleans & (int8_t)(pow(2, (counter >> 27) - (_position + 1)) - 1))) | (((counter >> 27) - 1) << 27);
 			break;
 
 		case 8:
-			if (this->counter >> 58 >= _position) 
-				this->bits = ((((this->booleans & (((T)0x01FFFFFFFFFFFFFF >> ((this->counter >> 58) - _position)) << ((this->counter >> 58) - _position)))) >> 1) | (this->booleans & (char)(pow(2, (this->counter >> 58) - (_position + 1)) - 1))) | (((this->counter >> 58) - 1) << 58);
-			break;
-
-		default:
+			if (counter >> 58 >= _position) 
+				bits = ((((booleans & (((T)0x01FFFFFFFFFFFFFF >> ((counter >> 58) - _position)) << ((counter >> 58) - _position)))) >> 1) | (booleans & (int8_t)(pow(2, (counter >> 58) - (_position + 1)) - 1))) | (((counter >> 58) - 1) << 58);
 			break;
 		}
 
 		std::bitset<sizeof(T) * 8> a(values);
 		std::cout << "After removing bit at position \"" << (int)_position << "\": " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// Replace element at position
-	void replace(const unsigned char _position, bool _newValue)
+	void replace(const uint8_t _position, bool _newValue) noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			if (this->counter >> 5 >= _position) 
-				this->bits = (this->bits & (((T)0xFF << ((this->counter >> 5) - _position)) + ((T)pow(2, (this->counter >> 5) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((this->counter >> 5) - 1) - _position)));
+			if (counter >> 5 >= _position) 
+				bits = (bits & (((T)0xFF << ((counter >> 5) - _position)) + ((T)pow(2, (counter >> 5) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((counter >> 5) - 1) - _position)));
 			break;
 
 		case 2:
-			if (this->counter >> 12 >= _position) 
-				this->bits = (this->bits & (((T)0xFFFF << ((this->counter >> 12) - _position)) + ((T)pow(2, (this->counter >> 12) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((this->counter >> 12) - 1) - _position)));
+			if (counter >> 12 >= _position) 
+				bits = (bits & (((T)0xFFFF << ((counter >> 12) - _position)) + ((T)pow(2, (counter >> 12) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((counter >> 12) - 1) - _position)));
 			break;
 
 		case 4:
-			if (this->counter >> 27 >= _position) 
-				this->bits = (this->bits & (((T)0xFFFFFFFF << ((this->counter >> 27) - _position)) + ((T)pow(2, (this->counter >> 27) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((this->counter >> 27) - 1) - _position)));
+			if (counter >> 27 >= _position) 
+				bits = (bits & (((T)0xFFFFFFFF << ((counter >> 27) - _position)) + ((T)pow(2, (counter >> 27) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((counter >> 27) - 1) - _position)));
 			break;
 
 		case 8:
-			if (this->counter >> 58 >= _position) 
-				this->bits = (this->bits & (((T)0xFFFFFFFFFFFFFFFF << ((this->counter >> 58) - _position)) + ((T)pow(2, (this->counter >> 58) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((this->counter >> 58) - 1) - _position)));
-			break;
-
-		default:
+			if (counter >> 58 >= _position) 
+				bits = (bits & (((T)0xFFFFFFFFFFFFFFFF << ((counter >> 58) - _position)) + ((T)pow(2, (counter >> 58) - (_position + 1)) - 1))) | ((T)(_newValue & 1) << ((((counter >> 58) - 1) - _position)));
 			break;
 		}
 
 		std::bitset<sizeof(T) * 8> a(values);
-		std::cout << "After replacing the bit at position \"" << (int)_position << "\" with the value \"" << (int)_newValue << "\": " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "After replacing the bit at position \"" << (uint16_t)_position << "\" with the value \"" << (uint16_t)_newValue << "\": " << a << "\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// Clear all data
-	void clear()
+	void clear() noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			this->bits >>= 8;
+			bits >>= 8;
 			break;
 
 		case 2:
-			this->bits >>= 16;
+			bits >>= 16;
 			break;
 
 		case 4:
-			this->bits >>= 32;
+			bits >>= 32;
 			break;
 
 		case 8:
-			this->bits >>= 64;
+			bits >>= 64;
 			break;
 
 		default:
@@ -321,33 +306,30 @@ public:
 
 		std::bitset<sizeof(T) * 8> a(values);
 		std::cout << "After clearing the byte: " << a << "\n";
-		std::cout << "Numerical value of counter: " << this->size() << "\n\n";
+		std::cout << "Numerical value of counter: " << size() << "\n\n";
 	}
 
 	// To get the amount of elements
-	unsigned int size()
+	uint16_t size() noexcept
 	{
 		switch (sizeof(T))
 		{
 		case 1:
-			return (unsigned int)(this->counter >> 5);
+			return (uint32_t)(counter >> 5);
 
 		case 2:
-			return (unsigned int)(this->counter >> 12);
+			return (uint32_t)(counter >> 12);
 
 		case 4:
-			return (unsigned int)(this->counter >> 27);
+			return (uint32_t)(counter >> 27);
 
 		case 8:
-			return (unsigned int)(this->counter >> 58);
-
-		default:
-			return 0;
+			return (uint32_t)(counter >> 58);
 		}
 	}
 
 	// To get the maximum allowed elements
-	const unsigned int MAX()
+	const uint16_t MAX() noexcept
 	{
 		switch (sizeof(T))
 		{
@@ -362,9 +344,6 @@ public:
 
 		case 8:
 			return 58;
-
-		default:
-			return 0;
 		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -3,47 +3,15 @@
 
 int main(void)
 {
-    EW_BitArray<unsigned long long> boolArray;
+    EW_BitArray<uint16_t> boolArray;
 
-    boolArray.push_back(false);
-    boolArray.push_back(true);
-    boolArray.push_back(false);
-    boolArray.push_back(true);
-    boolArray.push_back(false);
+    boolArray.add(false);
+    boolArray.add(true);
+    boolArray.add(false);
+    boolArray.add(true);
+    boolArray.add(false);
 
-    for (int i = 0; i < boolArray.size(); i++) std::cout << boolArray[i];
+    std::cout << boolArray.size();
 
-    EW_BitArray<unsigned char> boolArray2{ 0b11111, 5 };
-    if (!boolArray[0]) std::cout << sizeof(boolArray);
-
-    boolArray.pop_back();
-    //===============================================================
-
-    std::vector<bool> boolVector;
-
-    boolVector.push_back(false);
-    boolVector.push_back(true);
-    boolVector.push_back(false);
-    boolVector.push_back(true);
-    boolVector.push_back(false);
-
-    for (int i = 0; i < boolVector.size(); i++) std::cout << boolVector[i];
-
-    std::vector<bool> boolVector2{ 1, 1, 1, 1, 1 };
-    if (!boolVector[0]) std::cout << sizeof(boolVector);
-
-    boolVector.pop_back();
-    //===============================================================
-
-    std::vector<EW_BitArray<unsigned long long>> combinedArray;
-
-    for (int i = 0; i < 100; i++)
-    {
-        combinedArray.push_back(EW_BitArray<unsigned long long>{});
-
-        for (int j = 0; j < combinedArray[i].MAX(); j++)
-        {
-            combinedArray[i].push_back(true);
-        }
-    }
+    EW_BitArray<uint8_t> boolArray2{ 5, 0b11111 };
 } 
